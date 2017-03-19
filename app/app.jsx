f550fcf9af5fcd2ed713e3e654299ca3144baccc
Anh Le <lequocanh88@gmail.com>
2017-03-19 16:04:51 +0700

    finish boilerplate 3

--- app/app.jsx
+++ app/app.jsx
@@ -1,18 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-var Main = require('Main');
 
 // Load foundation
 //require('style!css!foundation-sites/dist/css/foundation.min.css'); - remove this line after adding 'sassLoader' obj in webpack.config
 $(document).foundation();
 //load app/styles/app.css
 require('style!css!sass!applicationStyles');{/*load alias applicationStyles in webpack.config by using style loader, css loader and sass loaders*/}
-
 ReactDOM.render(
-	<Router history={hashHistory}>
-		<Route path="/" component={Main}>			
-		</Route>
-	</Router>,
+	<p>React BoilerPlate 3</p>,
 	document.getElementById('app')
 );
